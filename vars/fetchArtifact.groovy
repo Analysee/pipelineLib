f1463bdb88ca2a2ce24d20da6d40890ef3f7732b
Dom, Dennis <Dennis.Dom@ordina.be>
2018-10-26 14:15:43 +0200

    ropger

--- vars/fetchArtifact.groovy
+++ vars/fetchArtifact.groovy
@@ -1,3 +1,3 @@
-def call(string repository,string artifactName,string version){
+def call(String repository,String artifactName,String version){
 sh 'curl -o fetchedArtifact.war  http://jenkins5lindacare.westeurope.cloudapp.azure.com:8081/repository/${artifactName}/com/rutvikpatel/springboot/helloworld/${artifactName}/${version}/${artifactName}-${version}.war'
 }
