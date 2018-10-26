d712d48219f9698c677462352f21438affc4b429
Dom, Dennis <Dennis.Dom@ordina.be>
2018-10-26 12:47:47 +0200

    ropger

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,9 +1,7 @@
-node {
-	stage('SonarQube analysis') {
+	def SonarQubeAnalysis() {
     withSonarQubeEnv('sonar') {
 	  withMaven(maven: 'maven'){
       sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
 	  }
-	}
 	}
 }
