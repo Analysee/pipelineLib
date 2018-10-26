62726dce4cff71d561018942d45e9c36ef927ba9
Dom, Dennis <Dennis.Dom@ordina.be>
2018-10-26 14:59:00 +0200

    ropger

--- vars/promoteArtifact.groovy
+++ vars/promoteArtifact.groovy
@@ -5,11 +5,11 @@ def call(String artifactName,String version,String stagingRepository2,String rel
         artifactId: artifactName,
         version:version,
         extension: 'war',
-        stagingRepository: 'http://jenkins5lindacare.westeurope.cloudapp.azure.com:8081/repository/${stagingRepository2}',
+        stagingRepository: 'http://jenkins5lindacare.westeurope.cloudapp.azure.com:8081/repository/lindacaretest',
         stagingUser: 'admin',
         stagingPW: 'admin123',
         skipDeletion: true,
-        releaseRepository: 'http://jenkins5lindacare.westeurope.cloudapp.azure.com:8081/repository/${releaseRepository2}',
+        releaseRepository: 'http://jenkins5lindacare.westeurope.cloudapp.azure.com:8081/repository/lindacareint',
         releaseUser: 'admin',
         releasePW: 'admin123'
     ) 
