a91d573287a19c464a8842a9db7f1db9c9175178
Dom, Dennis <Dennis.Dom@ordina.be>
2018-10-26 13:23:04 +0200

    ropger

--- vars/nexusArtifactUpload.groovy
+++ vars/nexusArtifactUpload.groovy
@@ -1,3 +1,3 @@
-def nexusArtifactUpload(String version, String artifactName){
+def call(String version, String artifactName){
      nexusPublisher nexusInstanceId: 'Nexus1', nexusRepositoryId: 'azuremaven', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'target/ROOT.war']], mavenCoordinate: [artifactId: artifactName, groupId: 'testEnvironment', packaging: 'war',version:version]]]
 }
