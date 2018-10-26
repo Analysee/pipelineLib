142c7b107c8c95c3534876c8ab8bbc14f3e24e46
Deckx, Kevin <Kevin.Deckx@ordina.be>
2018-10-26 12:51:50 +0200

    asdfasdf

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,4 +1,4 @@
-	def SonarQubeAnalysis() {
+def SonarQubeAnalysis() {
     withSonarQubeEnv('sonar') {
 	  withMaven(maven: 'maven'){
       sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
